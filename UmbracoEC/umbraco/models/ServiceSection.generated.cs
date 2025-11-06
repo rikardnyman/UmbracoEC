1ab00914192468dd4feefd42ee68821160c91391
Rikard <rikard.nyman@hotmail.com>
2025-11-06 13:12:44 +0100

    Made some minor tweaks

--- UmbracoEC/umbraco/models/ServiceSection.generated.cs
+++ UmbracoEC/umbraco/models/ServiceSection.generated.cs
@@ -50,6 +50,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
+		/// Section
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("section")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel Section => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "section");
+
+		///<summary>
 		/// Label
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
